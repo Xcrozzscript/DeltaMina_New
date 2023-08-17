72864ece61de15f9a877a9a05e92adbf78f9ef0e
= <=>
2023-08-18 01:05:36 +0700

    huhah

--- Record Data.php
+++ Record Data.php
@@ -5,11 +5,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 $mpdf = new \Mpdf\Mpdf();
 
 
-$Cetak=file_get_contents("cetak.php");
+$Cetak = file_get_contents("cetak.php");
 
 $mpdf->WriteHTML($Cetak);
 $mpdf->Output('Record Data Bulanan');
 ?>
-
-
-//ngepush bagai quda
